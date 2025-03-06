ff00897cbc236cf80702f39cfe8f3deb2762d5b3
jundory <ykwanlee98@naver.com>
2025-03-06 11:08:10 +0900

    Feat: 사이드바 아이콘 추가 및 미디어 쿼리로 덮이게 수정

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,3 +1,15 @@
 <template>
-  <div class="text-xl font-bold underline">SideBar위치</div>
+  <div class="flex flex-col">
+    <div class="flex flex-col items-center">
+      <HomeIcon class="w-6 text-black" />
+      <div class="">홈</div>
+    </div>
+  </div>
 </template>
+<script lang="ts" setup>
+// import {computed } from 'vue'
+// import { useRoute } from 'vue-router'
+import { HomeIcon } from '@heroicons/vue/24/outline'
+
+// const isWatchPage = computed(() => route.path.startsWith("/watch"));
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import BaseLayout from '@/layouts/BaseLayout.vue'
 import VideoList from '@/views/VideoList.vue'
 import ShortList from '@/views/ShortList.vue'
-import watchVideo from '@/views/watchVideo.vue'
+import WatchVideo from '@/views/WatchVideo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +14,7 @@ const router = createRouter({
       children: [
         { path: '', component: VideoList },
         { path: '/short', component: ShortList },
-        { path: '/watch', component: watchVideo },
+        { path: '/watch', component: WatchVideo },
       ],
     },
   ],
